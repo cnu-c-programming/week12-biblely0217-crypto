9ab3eb1e95854c070149ced1ead3afa61536aa31
bible <biblely0217@gmail.com>
2026-05-22 09:56:25 +0900

    3-5

--- code/3-5.c
+++ code/3-5.c
@@ -8,11 +8,29 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
+    int num;
     int sum = 0;
 
+    while (1) {
+        int result = fscanf(fp, "%d", &num);
+
+        if (result == EOF) {
+            break;
+        }
+
+        if (result == 1) {
+            sum += num;
+        }
+        else {
+            char buf[256];
+            fscanf(fp, "%s", buf);
+            fprintf(stderr, "invalid value: %s\n", buf);
+        }
+    }
 
     printf("sum: %d\n", sum);
+
     fclose(fp);
+
     return 0;
 }
